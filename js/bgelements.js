79d3ed946460c492c52f1580cb0efa6bc143fc88
Nenormalnui <shipulin002@gmail.com>
2022-09-25 19:46:16 +0300

    created functions renderTopAnime and renderAnimeList in mainData.js

--- js/bgelements.js
+++ js/bgelements.js
@@ -1,10 +1,13 @@
 const bgElements = () => {
   const elements = document.querySelectorAll(".set-bg");
 
-  for (let i = 0; i < elements.length; i++) {
+/*   for (let i = 0; i < elements.length; i++) {
     const src = elements[i].dataset.setbg;
     elements[i].style.backgroundImage = `url(${src})`;
-  }
+  } */
+  elements.forEach((elem) => {
+    elem.style.backgroundImage = `url(${elem.dataset.setbg})`;
+  });
 };
 
 bgElements();
